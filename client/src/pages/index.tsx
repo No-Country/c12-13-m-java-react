68b0856c52a206f42dc1e695666a3c47a89bd9eb
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-06-28 21:57:05 -0300

    home test

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,25 @@
 import { Main } from "@/components";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Main>
+      <section className="flex flex-col">
       <h1>Home</h1>
+      <h2 className="text-2xl text-red-700">Hola, bienvenido a nuestro deploy</h2>
+  <div  className="flex flex-col">
+    <h3 className="text-2xl text-red-700 mt-6">Links</h3>
+    <Link href="/">
+      Home
+    </Link>
+    <Link href="/about">
+      About
+    </Link>
+    <Link href="/help">
+      Help
+    </Link>
+  </div>
+  </section>
     </Main>
   );
 }
