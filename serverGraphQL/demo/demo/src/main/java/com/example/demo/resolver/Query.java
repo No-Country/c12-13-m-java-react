fceced7e35d0d84714619db3cfd0597714a6dce5
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-07-05 23:46:43 -0300

    server graphql andando

---
+++ serverGraphQL/demo/demo/src/main/java/com/example/demo/resolver/Query.java
@@ -0,0 +1,39 @@
+package com.example.demo.resolver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Author;
+import com.example.demo.model.Tutorial;
+import com.example.demo.repository.AuthorRepository;
+import com.example.demo.repository.TutorialRepository;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+  private AuthorRepository authorRepository;
+  private TutorialRepository tutorialRepository;
+
+  @Autowired
+  public Query(AuthorRepository authorRepository, TutorialRepository tutorialRepository) {
+    this.authorRepository = authorRepository;
+    this.tutorialRepository = tutorialRepository;
+  }
+
+  public Iterable<Author> findAllAuthors() {
+    return authorRepository.findAll();
+  }
+
+  public Iterable<Tutorial> findAllTutorials() {
+    return tutorialRepository.findAll();
+  }
+
+  public long countAuthors() {
+    return authorRepository.count();
+  }
+
+  public long countTutorials() {
+    return tutorialRepository.count();
+  }
+
+}
