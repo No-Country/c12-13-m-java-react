3857fb0c5dace9963b99be8ee4e9636fe187a2e9
Christian Leon <106689802+VegettoJesus@users.noreply.github.com>
2023-07-10 12:04:50 -0500

    Spring Boot cloudinary

--- serverGraphQL/demo/demo/src/main/java/com/example/demo/resolver/UserController.java
+++ serverGraphQL/demo/demo/src/main/java/com/example/demo/resolver/UserController.java
@@ -3,21 +3,47 @@ package com.example.demo.resolver;
 import com.example.demo.model.Space;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.CloudinaryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
+import java.util.Map;
+import java.io.IOException;
 import java.util.Date;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    CloudinaryService cloudinaryService;
+
     private final UserRepository userRepository;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, CloudinaryService cloudinaryService) {
         this.userRepository = userRepository;
+        this.cloudinaryService = cloudinaryService;
     }
+
+    /*@PostMapping("/upload")
+    public ResponseEntity<Map> upload(@RequestParam MultipartFile multipartFile) throws IOException{
+        Map result = cloudinaryService.upload(multipartFile);
+        return new ResponseEntity(result, HttpStatus.OK);
+    } 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Map> upload(@RequestParam("id") String id) throws IOException{
+        Map result = cloudinaryService.delete(id);
+        return new ResponseEntity(result, HttpStatus.OK);
+    }*/
 
     @SchemaMapping(typeName = "Query", value = "findAllUsers")
     public List<User> findAll() {
@@ -42,7 +68,7 @@ public class UserController {
             @Argument String password,
             @Argument String loginMethod,
             @Argument String username,
-            @Argument String profileImage,
+            @Argument MultipartFile profileImage,
             @Argument Boolean isSuperAdmin) {
         User user = new User();
         user.setFirstName(firstName);
@@ -59,9 +85,20 @@ public class UserController {
         }
         user.setPassword(password);
         user.setLoginMethod(loginMethod);
+        String profileImageUrl = null;
         if (profileImage != null && !profileImage.isEmpty()) {
-            user.setProfileImage(profileImage);
+            try {
+                Map profileImageUploadResult = cloudinaryService.upload(profileImage);
+                profileImageUrl = (String) profileImageUploadResult.get("url");
+            } catch (IOException e) {
+                // Manejar el error de carga de imagen si es necesario
+                e.printStackTrace();
+            } 
         }
+        if (profileImageUrl != null) {
+            user.setProfileImage(profileImageUrl);
+        }
+
         if (isSuperAdmin != null && !isSuperAdmin) {
             user.setIsSuperAdmin(isSuperAdmin);
         }
